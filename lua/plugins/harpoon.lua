616fb8e199bbd7058af589743753c63a56a1c6b9
João Inez <joaoinez.info@gmail.com>
2024-12-07 23:03:39 +0100

    Add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = {
+    {
+      '<leader>a',
+      function() require('harpoon'):list():add() end,
+      desc = 'Harpoon File',
+    },
+    {
+      '<leader>fh',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'Harpoon Quick Menu',
+    },
+    {
+      '<M-u>',
+      function() require('harpoon'):list():select(1) end,
+    },
+    {
+      '<M-i>',
+      function() require('harpoon'):list():select(2) end,
+    },
+    {
+      '<M-o>',
+      function() require('harpoon'):list():select(3) end,
+    },
+    {
+      '<M-p>',
+      function() require('harpoon'):list():select(4) end,
+    },
+  },
+}
