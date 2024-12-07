8dbc04d403291ace769c2a0c43cdfd31557001b0
João Inez <joaoinez.info@gmail.com>
2024-12-07 16:57:05 +0100

    Add telescope

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
     'echasnovski/mini.nvim',
     version = false,
     config = function()
-      require('mini.icons').setup()
+      require('mini.icons').setup { enabled = vim.g.have_nerd_font }
     end,
   },
 }
