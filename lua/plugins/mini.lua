b034d8f5341dc71fe4b294d99902508659fa9910
João Inez <joaoinez.info@gmail.com>
2024-12-07 19:23:42 +0100

    Split lsp file

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,32 @@
 return {
-  { 'echasnovski/mini.icons', version = false, lazy = true, opts = {} },
+  {
+    'echasnovski/mini.icons',
+    version = false,
+    lazy = true,
+    enabled = vim.g.have_nerd_font,
+    opts = {},
+  },
   {
     'echasnovski/mini.splitjoin',
     version = false,
     event = 'VeryLazy',
-    enabled = vim.g.have_nerd_font,
     opts = {},
+  },
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    event = 'VeryLazy',
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { 'string' },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    },
   },
 }
