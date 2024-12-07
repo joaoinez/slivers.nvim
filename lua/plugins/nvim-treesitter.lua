87fd84608e4b20ab62c293b840f4b1bd72b2d50d
João Inez <joaoinez.info@gmail.com>
2024-12-07 19:41:59 +0100

    Clean comments

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs',
-  event = { 'BufRead' },
+  event = { 'BufRead', 'VeryLazy' },
   lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
   -- dependencies = {
   --   "windwp/nvim-ts-autotag",
