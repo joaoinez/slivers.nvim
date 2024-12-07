03e2d0db325d96502b52b24b13f84b9a92e22729
João Inez <joaoinez.info@gmail.com>
2024-12-07 17:16:35 +0100

    Add treesitter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,8 @@ return {
     'echasnovski/mini.nvim',
     version = false,
     config = function()
-      require('mini.icons').setup { enabled = vim.g.have_nerd_font }
+      require('mini.icons').setup { enabled = vim.g.have_nerd_font } -- Nerd icons.
+      require('mini.splitjoin').setup() -- Toggle split arrays and objects with `gS`.
     end,
   },
 }
