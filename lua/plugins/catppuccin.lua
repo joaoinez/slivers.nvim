4dd05921f6960c041e0adc80ace74827facb563e
João Inez <joaoinez.info@gmail.com>
2024-12-10 23:52:58 +0100

    Improve line numbers color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -42,16 +42,12 @@ return {
       treesitter_context = true,
       which_key = true,
     },
+    highlight_overrides = {
+      all = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 },
+        }
+      end,
+    },
   },
-  -- specs = {
-  --   {
-  --     "akinsho/bufferline.nvim",
-  --     optional = true,
-  --     opts = function(_, opts)
-  --       if (vim.g.colors_name or ""):find("catppuccin") then
-  --         opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-  --       end
-  --     end,
-  --   },
-  -- },
 }
