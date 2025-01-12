e123c58cb47433e9674c5ca404d61d02a0de0470
João Inez <joaoinez.info@gmail.com>
2025-01-12 05:10:55 +0100

    Add mini.pick

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -1,6 +1,7 @@
 return {
   require 'plugins.mini.ai',
   require 'plugins.mini.pairs',
+  require 'plugins.mini.pick',
   require 'plugins.mini.splitjoin',
   require 'plugins.mini.surround',
 }
---
+++ lua/plugins/mini/pick.lua
@@ -0,0 +1,25 @@
+return {
+  'echasnovski/mini.pick',
+  enabled = true,
+  version = false,
+  event = 'VeryLazy',
+  opts = function()
+    local win_config = function()
+      local height = math.floor(0.618 * vim.o.lines)
+      local width = math.floor(0.618 * vim.o.columns)
+      return {
+        anchor = 'NW',
+        height = height,
+        width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
+      }
+    end
+
+    return {
+      window = {
+        config = win_config(),
+      },
+    }
+  end,
+}
