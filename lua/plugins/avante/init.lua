d86d474fc93125cd8b924c25525e6285f743f346
João Inez <joaoinez.info@gmail.com>
2025-01-27 10:54:22 +0100

    fix: Update Avante plugin settings and mappings.

--- lua/plugins/avante/init.lua
+++ lua/plugins/avante/init.lua
@@ -19,26 +19,25 @@ return {
     },
   },
   opts = {
-    provider = 'deepseek_coder',
-    auto_suggestions_provider = 'qwen',
+    provider = 'qwen_coder',
+    auto_suggestions_provider = 'qwen_mini',
     behaviour = {
       auto_suggestions = false,
     },
     vendors = {
-      deepseek_coder = {
+      qwen_coder = {
         __inherited_from = 'openai',
         api_key_name = '',
         endpoint = 'http://127.0.0.1:11434/v1',
-        -- model = 'deepseek-coder-v2:16b',
         model = 'qwen2.5-coder:14b',
       },
       deepseek_r1 = {
         __inherited_from = 'openai',
         api_key_name = '',
         endpoint = 'http://127.0.0.1:11434/v1',
-        model = 'deepseek-r1:14b',
+        model = 'deepseek-r1:7b',
       },
-      qwen = {
+      qwen_mini = {
         __inherited_from = 'openai',
         api_key_name = '',
         endpoint = 'http://127.0.0.1:11434/v1',
@@ -46,15 +45,20 @@ return {
       },
     },
     dual_boost = {
-      enabled = false,
+      enabled = vim.fn.has 'macunix' ~= 1,
       first_provider = 'deepseek_r1',
-      second_provider = 'deepseek_coder',
-      prompt = 'Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]',
-      timeout = 300000, -- Timeout in milliseconds
+      second_provider = 'qwen_coder',
+      prompt = 'Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]',
+      timeout = 600000, -- Timeout in milliseconds
     },
     windows = {
       input = {
         prefix = ' ',
+      },
+    },
+    mappings = {
+      toggle = {
+        default = '<leader>ta',
       },
     },
   },
@@ -108,8 +112,8 @@ return {
               desc = 'Toggle Suggestion',
             },
             {
-              '<leader>at',
-              desc = 'Toggle Chat',
+              '<leader>ta',
+              desc = 'Toggle AI',
             },
           }, { notify = false })
         end
