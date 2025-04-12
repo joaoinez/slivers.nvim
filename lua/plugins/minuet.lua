e762627cae84c796bee3b12333b692b885bbb8ca
João Inez <joaoinez.info@gmail.com>
2025-04-12 03:53:45 +0200

    refactor: replace supermaven with minuet

---
+++ lua/plugins/minuet.lua
@@ -0,0 +1,39 @@
+---@type LazySpec
+return {
+  'milanglacier/minuet-ai.nvim',
+  enabled = true,
+  cond = not vim.g.vscode,
+  opts = {
+    provider = 'openai_compatible',
+    request_timeout = 2.5,
+    throttle = 1500,
+    debounce = 600,
+    provider_options = {
+      openai_compatible = {
+        api_key = 'OPENROUTER_API_KEY',
+        end_point = 'https://openrouter.ai/api/v1/chat/completions',
+        model = 'mistralai/codestral-2501',
+        -- model = 'google/gemini-2.0-flash-001',
+        name = 'Openrouter',
+        optional = {
+          max_tokens = 128,
+          top_p = 0.9,
+          provider = {
+            sort = 'throughput',
+          },
+        },
+      },
+    },
+    virtualtext = {
+      auto_trigger_ft = { '*' },
+      keymap = {
+        accept = '<A-y>',
+        accept_line = '<Tab>',
+        prev = '<A-[>',
+        next = '<A-]>',
+        dismiss = '<A-e>',
+      },
+    },
+    n_completions = 1,
+  },
+}
--- lua/plugins/supermaven.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  'supermaven-inc/supermaven-nvim',
-  enabled = true,
-  cond = not vim.g.vscode,
-  event = 'InsertEnter',
-  opts = {},
-}
