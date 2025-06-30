679c3ec009da2f6e755facc114d474919e54e328
João Inez <joaoinez.info@gmail.com>
2025-06-30 09:34:50 +0100

    refactor: neocodeium manual trigger

--- lua/plugins/neocodeium.lua
+++ lua/plugins/neocodeium.lua
@@ -1,9 +1,15 @@
+---@type LazySpec
 return {
   'monkoose/neocodeium',
+  enabled = true,
+  cond = not vim.g.vscode,
   event = { 'VeryLazy', 'InsertEnter' },
   config = function()
     local neocodeium = require 'neocodeium'
-    neocodeium.setup { silent = true }
-    vim.keymap.set('i', '<tab>', neocodeium.accept)
+
+    neocodeium.setup { silent = true, manual = true }
+
+    vim.keymap.set('i', '<s-tab>', neocodeium.accept)
+    vim.keymap.set('i', '<m-l>', function() neocodeium.cycle_or_complete(1) end)
   end,
 }
