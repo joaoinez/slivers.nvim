f50b4aa778cd0e838704307d515a38029521c279
João Inez <joaoinez.info@gmail.com>
2025-12-02 18:03:49 +0100

    refactor: rename xcode theme author name

--- lua/plugins/colorschemes/xcode.lua
+++ lua/plugins/colorschemes/xcode.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
-  'arzg/vim-colors-xcode',
+  'lunacookies/vim-colors-xcode',
   enabled = true,
   priority = 1000,
   lazy = Slivers.colorscheme.get_config(ColorSliver.colorschemes, vim.g.colors_name).name ~= 'xcode',
